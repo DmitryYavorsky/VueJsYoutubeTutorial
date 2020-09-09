7f8b3bd5e8aa43957ae7c8eaac92010ee152a87a
dmitriy <dimonslayerrooock@mail.ru>
2020-09-09 12:12:44 +0300

    base pages

--- src/router/index.js
+++ src/router/index.js
@@ -3,28 +3,61 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
-
+const main = "main";
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    {
+        path: "/",
+        name: "Home",
+        component: Home
+    },
+    {
+        path: "/login",
+        name: "login",
+        meta: {layout: "empty"},
+        component: () => import("../views/Login.vue")
+    },
+    {
+        path: "/register",
+        name: "register",
+        meta: {layout: "empty"},
+        component: () => import("../views/Register")
+    },
+    {
+        path: "/categories",
+        name: "categories",
+        meta: {layout: main},
+        component: () => import("../views/Categories.vue")
+    },
+    {
+        path: "/history",
+        name: "history",
+        meta: {layout: main},
+        component: () => import("../views/History")
+    },
+    {
+        path: "/planning",
+        name: "planning",
+        meta: {layout: main},
+        component: () => import("../views/Planning")
+    },
+    {
+        path: "/profile",
+        name: "profile",
+        meta: {layout: main},
+        component: () => import("../views/Profile")
+    },
+    {
+        path: "/record",
+        name: "record",
+        meta: {layout: main},
+        component: () => import("../views/Record")
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes
 });
 
 export default router;
